f4c10a0100ad069fdd65b08d1fa5684524660ffc
JamesWongAndroid <james.wong.android@gmail.com>
2014-10-20 13:02:07 -0400

    tutorial finished
    A “working” form that saves to the database.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,7 +11,19 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $request = $this->getRequest();
     	$form = new Application_Form_Index();
+
+    	if ($request->isPost()) {
+    		if ($form->isValid($request->getPost())) {
+    			$maillist = new Application_Model_MailList($form->getValues());
+    			$mapper = new Application_Model_MailListMapper();
+    			$mapper->save($maillist);
+
+    			$this->_helper->redirector('I did it!');
+    		} 
+    	}
+
         $this->view->form = $form;
     }
 
